54f95d974d969b18566e48f3b7a926d04609fffc
flaviosoliver <flavsoliver@gmail.com>
2023-03-21 01:29:07 -0300

    upload project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+import Routes from './services/Routes';
+
+function App() {
+  return (
+    <Router>
+      <div>Movie Card Library CRUD</div>
+      <Routes />
+    </Router>
+  );
+}
+
+export default App;
